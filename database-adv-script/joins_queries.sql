c3e52fa45a9a57e8d11a559a5b8a430877afdbd4
AbenezerAbera7 <abenezer.abera623@gmail.com>
2025-07-13 22:37:24 +0300

    Write a query using an INNER JOIN, LEFT JOIN, and FULL OUTER JOIN

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,44 @@
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM 
+    booking b
+INNER JOIN 
+    "user" u ON b.user_id = u.user_id;
+
+
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.price_per_night,
+    r.review_id,
+    r.rating,
+    r.comment,
+    r.created_at AS review_date
+FROM 
+    property p
+LEFT JOIN 
+    review r ON p.property_id = r.property_id;
+
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.status
+FROM 
+    "user" u
+FULL OUTER JOIN 
+    booking b ON u.user_id = b.user_id;
