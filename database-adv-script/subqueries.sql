391a7f05e07fa6009eb53243dad44cccb66b71dd
AbenezerAbera7 <abenezer.abera623@gmail.com>
2025-07-13 22:45:24 +0300

    Write both correlated and non-correlated subqueries.

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,16 +1,39 @@
 SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.price_per_night,
+    avg_rating.average_rating
+FROM 
+    property p
+JOIN (
+    SELECT 
+        property_id, 
+        AVG(rating) AS average_rating
+    FROM 
+        review
+    GROUP BY 
+        property_id
+    HAVING 
+        AVG(rating) > 4.0
+) avg_rating ON p.property_id = avg_rating.property_id
+ORDER BY 
+    avg_rating.average_rating DESC;
+
+
+SELECT 
     u.user_id,
     u.first_name,
     u.last_name,
     u.email,
-    COUNT(b.booking_id) AS booking_count
+    (SELECT COUNT(*) 
+     FROM booking b 
+     WHERE b.user_id = u.user_id) AS booking_count
 FROM 
     "user" u
-JOIN 
-    booking b ON u.user_id = b.user_id
-GROUP BY 
-    u.user_id
-HAVING 
-    COUNT(b.booking_id) > 3
+WHERE 
+    (SELECT COUNT(*) 
+     FROM booking b 
+     WHERE b.user_id = u.user_id) > 3
 ORDER BY 
     booking_count DESC;
